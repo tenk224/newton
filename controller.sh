205c0f9c89a10a3ba9695bef9a5835549a14c0e1
tenk224 <equal2603@gmail.com>
2017-03-29 03:32:36 +0700

    c

--- controller.sh
+++ controller.sh
@@ -37,24 +37,7 @@ EOF
 systemctl enable mariadb.service
 systemctl start mariadb.service
 
-spawn mysql_secure_installation
-expect {Enter current password for root (enter for none):}
-send "\r"
-expect {Change the root password?}
-send "Y\r"
-expect {New password:}
-send "123456\r"
-expect {Re-enter new password:}
-send "123456\r"
-expect {Remove anonymous users?}
-send "Y\r"
-expect {Disallow root login remotely?}
-send "Y\r"
-expect {Remove test database and access to it?}
-send "Y\r"
-expect {Reload privilege tables now?}
-send "Y\r"
-expect eof
+mysql_secure_installation
 
 mysql -u "root" "-p123456" < ./db.sql
 #----------------------------------------------------------------------------------
