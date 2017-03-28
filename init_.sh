e5a8586ec34f9d9db4f99e620988a1c998852110
tenk224 <equal2603@gmail.com>
2017-03-29 02:46:57 +0700

    c

--- init_.sh
+++ init_.sh
@@ -7,6 +7,11 @@ systemctl disable firewalld
 systemctl stop NetworkManager
 systemctl disable NetworkManager
 
+yum install centos-release-openstack-newton -y
+yum upgrade -y
+yum install python-openstackclient \
+openstack-selinux -y
+
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
@@ -15,8 +20,3 @@ sed -i 's/saved/0/g' /etc/default/grub
 sed -i 's/5/0/g' /etc/default/grub
 
 grub2-mkconfig -o /boot/grub2/grub.cfg
-
-#yum install centos-release-openstack-newton -y
-#yum upgrade -y
-#yum install python-openstackclient \
-#openstack-selinux -y
